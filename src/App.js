ecd863b9011b415d2ad2797e9e20d43f87041db5
Tara Sankhalpara <tsankhalpara@mac.com>
2019-09-30 17:26:29 +0100

    add props to counter function

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ const Player = (props) => {
   );
 }
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">20</span>
+      <span className="counter-score">{props.score}</span>
       <button className="counter-action increment"> + </button>
     </div>
   );
