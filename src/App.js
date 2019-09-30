2ce7fb49de03e2e72aada91a245c8959f73eb208
Tara Sankhalpara <tsankhalpara@mac.com>
2019-09-30 14:28:34 +0100

    add player const

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 const Header = () => {
   return (
     <header>
       <h1>Scoreboard</h1>
       <span className="stats">Players: 1</span>
     </header>
+  );
+}
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Tara
+      </span>
+    </div>
   );
 }
 
