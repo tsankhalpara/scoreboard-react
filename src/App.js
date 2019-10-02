88eee4aac7d1a5e41bedef02bc13b27bfd781f13
Tara Sankhalpara <tsankhalpara@mac.com>
2019-10-02 18:53:09 +0100

    add functionality to button to remove player

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
-      <button className="remove-player" onClick={ () => props.removePlayer()}>X</button>
+      <button className="remove-player" onClick={ () => props.removePlayer(props.id)}>X</button>
         {props.name}
       </span>
       <Counter score={props.score}/>
