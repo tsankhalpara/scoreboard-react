7d916fda0e89f998cd9f78028b7d12994b89730b
Tara Sankhalpara <tsankhalpara@mac.com>
2019-10-02 15:42:06 +0100

    change counter const to class

--- src/App.js
+++ src/App.js
@@ -37,14 +37,16 @@ const Player = (props) => {
   );
 }
 
-const Counter = (props) => {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{props.score}</span>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
+class Counter extends React.Component {
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{ this.props.score }</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+  }
 }
 
 const App = () => {
